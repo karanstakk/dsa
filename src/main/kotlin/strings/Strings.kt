3f1a0a962f6b74c5949a36e4e94d0fdf592fb8cd
Karan Sharma <karansharma@Karans-MacBook-Pro.local>
2024-06-18 16:44:13 -0600

    LC: 846

--- src/main/kotlin/strings/Strings.kt
+++ src/main/kotlin/strings/Strings.kt
@@ -1,10 +1,8 @@
 package strings
 
+import java.util.PriorityQueue
+import kotlin.text.StringBuilder
 
-fun main() {
-
-    println(firstUniqChar("dddccdbba"))
-}
 
 // LC:387
 fun firstUniqChar(string: String): Int {
@@ -53,7 +51,7 @@ fun firstUniqChar(string: String): Int {
     return index
 }
 
-// Is Unique : Chapt:1.1
+// Is Unique : Chap:1.1
 
 fun isUnique(str : String) : Boolean {
 
@@ -77,4 +75,149 @@ fun isUnique(str : String) : Boolean {
     }
 
     return true
+}
+
+// LC:567,
+// Is s1 permutation of s2
+fun checkInclusion(s1: String, s2: String): Boolean {
+
+    if (s2.length < s1.length) {
+        return false
+    }
+
+    val window = s1.length
+
+    val sortedS1 = s1.toCharArray().sorted().toString()
+
+    print(sortedS1)
+
+    for(i in s2.indices) {
+
+        println(i)
+        val endIndex = i + window
+
+        if( i + window > s2.length) {
+            return false
+        }
+
+        val substring = s2.substring(i, endIndex).toCharArray().sorted().toString()
+
+        println(substring)
+
+        if (sortedS1 == substring)
+            return true
+
+    }
+
+    return false
+}
+
+// Chap:1.2
+// Is s1 permutation of s2
+fun checkPermutation(s1: String, s2: String): Boolean {
+
+    if (s1.length != s2.length) {
+        return false
+    }
+    return s1.toSortedSet() == s2.toSortedSet()
+}
+
+fun exist(board: Array<CharArray>, word: String): Boolean {
+    val row = board.size
+    val col = board[0].size
+    return hasValue(0,0, 0,  board, word, row, col)
+}
+
+fun hasValue(i : Int, j: Int, pos : Int, board: Array<CharArray>, word: String, row: Int, col : Int) : Boolean {
+
+    if ( i < 0 || j<0 || i>=row || j>=col) {
+        println("out of bound")
+        return false
+    }
+
+    val char = word[pos]
+
+    if (board[i][j] == char && pos == word.length-1) {
+        return true
+    }
+
+    println("here i "+i)
+    println("here j "+j)
+    println(char)
+    println(board[i][j])
+
+    if (board[i][j] == char) {
+
+        println("if i " + i)
+        println("if j " + j)
+        return hasValue(i + 1, j, pos + 1, board, word, row, col) ||
+                hasValue(i, j + 1, pos + 1, board, word, row, col) ||
+                hasValue(i, j - 1, pos + 1, board, word, row, col) ||
+                hasValue(i - 1, j, pos + 1, board, word, row, col)
+    } else if(j+1 < col  && pos == 0) {
+        return hasValue(i, j+1,  pos, board, word, row, col)
+    } else if (i+1 < row && pos == 0 ) {
+        return hasValue(i+1, j, pos,  board, word ,row, col)
+    }
+    return false
+}
+
+fun urlify(string: String, trueLength: Int) : String {
+    val result = StringBuilder()
+
+    for (i in string.indices) {
+        if (string[i] == ' ') {
+            result.append("%20")
+        } else {
+            result.append(string[i])
+        }
+    }
+    return result.toString()
+}
+
+fun isNStraightHand(hand: IntArray, groupSize: Int): Boolean {
+
+    if (hand.isEmpty() || hand.size < groupSize || hand.size%groupSize != 0)
+        return false
+
+    if (groupSize == 1 ) {
+        return true
+    }
+
+    val list = hand.sorted().toCollection(ArrayList())
+
+    while (list.size > 0) {
+
+        val currentNum = list[0]
+        list.removeAt(0)
+
+        for (i in currentNum+1 until  currentNum+groupSize ) {
+             if (!list.remove(i)) {
+                 return false
+             }
+        }
+    }
+    return true
+}
+
+fun isConsecutive(prev: Int, current : Int): Boolean {
+    return current - prev == 1
+}
+
+fun main() {
+
+//    println(firstUniqChar("dddccdbba"))
+//
+//    println(isUnique("abca"))
+//
+//    println(checkPermutation("abc", "bac"))
+
+//   val array = arrayOf(
+//        charArrayOf('A', 'B', 'C', 'E'),
+//        charArrayOf('S', 'F', 'C', 'S'),
+//        charArrayOf('A', 'D', 'E', 'E')
+//    )
+
+//    println(exist(array, "ABCB"))
+      println(isNStraightHand(intArrayOf(1,2,3,6,2,3,4,7,8),3))
 }
